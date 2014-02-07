cc3a6327da36f7786318ef01511a2208db3245a6
alexanderchurchill@gmail.com <alexanderchurchill@gmail.com>
2014-02-07 18:45:31 +0000

    param sweep

--- left_ones.py
+++ left_ones.py
@@ -9,7 +9,7 @@ from optimizers import sgd_optimizer
 from numpy import array as ar
 import pdb
 import distance
-import os
+import os,sys
 from ga import KnapsackData
 def ensure_dir(f):
     d = os.path.dirname(f)
@@ -52,11 +52,11 @@ class LeftOnes(object):
         sorted_fitnesses.reverse()
         return strings,[strings[i] for i in sorted_fitnesses[0:lim]]
 
-    def train_dA(self,data,corruption_level=0.2,num_epochs=200):
+    def train_dA(self,data,corruption_level=0.2,num_epochs=200,lr=0.1):
         train_data = data
         # pdb.set_trace()
         train_set = SequenceDataset(train_data,batch_size=20,number_batches=None)
-        sgd_optimizer(self.dA.params,[self.dA.input],self.dA.cost,train_set,lr=0.1,num_epochs=num_epochs)
+        sgd_optimizer(self.dA.params,[self.dA.input],self.dA.cost,train_set,lr=lr,num_epochs=num_epochs)
 
     def build_sample_dA(self):
         self.sample_dA = theano.function([self.dA.input],self.dA.sample)
@@ -109,13 +109,30 @@ class LeftOnes(object):
         pickle.dump(data,open("results/autoencoder/{0}.pkl".format(name),"wb"))
         return data
 
-    def iterative_algorithm(self,name,pop_size=100,genome_length=20,lim=20,trials=10,corruption_level=0.2,num_epochs=50):
+    def iterative_algorithm(
+        self,
+        name,
+        pop_size=100,
+        genome_length=20,
+        lim_percentage=20,
+        lim=20,
+        trials=10,
+        corruption_level=0.2,
+        num_epochs=50,
+        lr = 0.1,
+        online_training=False,
+        pickle_data=False,
+        max_evaluations=200000):
+        trials = max_evaluations/pop_size
+        population_limit = lim
+        if lim_percentage > 0:
+            population_limit = pop_size/lim_percentage
         fitfile = open("{0}_fitnesses.dat".format(name),"w")
         self.dA = dA(n_visible=genome_length,n_hidden=50)
         self.dA.build_dA(corruption_level)
         self.build_sample_dA()
-        all_strings,good_strings=self.generate_good_strings(pop_size,genome_length,lim)
-        self.train_dA(ar(good_strings),corruption_level=corruption_level,num_epochs=50)
+        all_strings,good_strings=self.generate_good_strings(pop_size,genome_length,population_limit)
+        self.train_dA(ar(good_strings),corruption_level=corruption_level,num_epochs=50,lr=lr)
         sampled_population = [lo.sample_dA([i]) for i in all_strings]
         original_fitnesses,sample_fitnesses,differences,distances = self.get_statistics(all_strings,sampled_population)
         data = {
@@ -124,18 +141,20 @@ class LeftOnes(object):
         "differences_in_fitness":differences,
         "distances":distances
         }
-        pickle.dump(data,open("results/autoencoder/{0}_0.pkl".format(name),"wb"))
-        fitfile.write("{0},{1},{2}\n".format(np.mean(original_fitnesses),np.min(original_fitnesses),np.max(original_fitnesses)))
-        fitfile.write("{0},{1},{2}\n".format(np.mean(sample_fitnesses),np.min(sample_fitnesses),np.max(sample_fitnesses)))
+        if pickle_data:
+            pickle.dump(data,open("results/autoencoder/{0}_0.pkl".format(name),"wb"))
+        fitfile.write("{0},{1},{2},{3}\n".format(np.mean(original_fitnesses),np.min(original_fitnesses),np.max(original_fitnesses),np.std(original_fitnesses)))
+        fitfile.write("{0},{1},{2},{3}\n".format(np.mean(sample_fitnesses),np.min(sample_fitnesses),np.max(sample_fitnesses),np.std(original_fitnesses)))
         print "{0},{1},{2}\n".format(np.mean(original_fitnesses),np.min(original_fitnesses),np.max(original_fitnesses))
         print "{0},{1},{2}\n".format(np.mean(sample_fitnesses),np.min(sample_fitnesses),np.max(sample_fitnesses))
         for iteration in range(0,trials):
             population = [z[0] for z in sampled_population]
-            self.dA = dA(n_visible=genome_length,n_hidden=50)
-            self.dA.build_dA(corruption_level)
-            self.build_sample_dA()
-            good_strings,good_strings_fitnesses=self.get_good_strings(population,lim)
-            self.train_dA(ar(good_strings),corruption_level=corruption_level,num_epochs=num_epochs)
+            if online_training:
+                self.dA = dA(n_visible=genome_length,n_hidden=50)
+                self.dA.build_dA(corruption_level)
+                self.build_sample_dA()
+            good_strings,good_strings_fitnesses=self.get_good_strings(population,population_limit)
+            self.train_dA(ar(good_strings),corruption_level=corruption_level,num_epochs=num_epochs,lr=lr)
             sampled_population = [lo.sample_dA([i]) for i in population]
             original_fitnesses,sample_fitnesses,differences,distances = self.get_statistics(population,sampled_population)
             data = {
@@ -144,8 +163,9 @@ class LeftOnes(object):
             "differences_in_fitness":differences,
             "distances":distances
             }
-            pickle.dump(data,open("results/autoencoder/{0}_{1}.pkl".format(name,iteration+1),"wb"))
-            fitfile.write("{0},{1},{2}\n".format(np.mean(sample_fitnesses),np.min(sample_fitnesses),np.max(sample_fitnesses)))
+            if pickle_data:
+                pickle.dump(data,open("results/autoencoder/{0}_{1}.pkl".format(name,iteration+1),"wb"))
+            fitfile.write("{0},{1},{2},{3}\n".format(np.mean(sample_fitnesses),np.min(sample_fitnesses),np.max(sample_fitnesses),np.std(original_fitnesses)))
             fitfile.flush()
             print "{0},{1},{2}\n".format(np.mean(sample_fitnesses),np.min(sample_fitnesses),np.max(sample_fitnesses))
         fitfile.close()
@@ -173,30 +193,38 @@ class ACSKnapsack(LeftOnes):
 
 
 if __name__ == '__main__':
-    # all_strings,good_strings=lo.generate_good_strings(10000)
-    # lo.fitness_many(good_strings)
-    # lo.train_dA(ar(good_strings))
-    # z = [lo.sample_dA([i]) for i in all_strings]
-    # plt.subplot(1,2,2)
-    # plt.title("new")
-    # plt.hist(lo.fitness_many([i[0] for i in z]))
-    # plt.subplot(1,2,1)
-    # plt.title("orginal")
-    # plt.hist(lo.fitness_many(all_strings))
-    # plt.show()
-    # f,d,dist = lo.calculate_changes_in_fitness(all_strings,10)
-    c_level = 0.1
-    # lo = LeftOnes(corruption_level=c_level)
-    # data =lo.experiment("c-{0}".format(c_level),no_trials=100,corruption_level=c_level)
-    # t= Test()
-    # lo.iterative_algorithm("test_p_100_0.2",trials=20,corruption_level=0.2)
-    # data=lo.fitness_many(all_strings)
-    # y,binEdges=np.histogram(data,bins=100)
-    # bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
-    # p.plot(bincenters,y,'-')
-    # p.show()
+    args = sys.argv
+    pop_size=int(args[1])
+    genome_length=105
+    lim_percentage=int(args[2])
+    lim=int(args[3])
+    trials=10
+    corruption_level=0.1
+    num_epochs=int(args[4])
+    lr = float(args[5])
+    online_training=int(args[6])
+    if online_training == 0:
+        online_training = False
+    else:
+        online_training = True
+    pickle_data=False
     for i in range(0,10):
-        lo = ACSKnapsack(corruption_level=c_level,knapsack_file="weing8.pkl")
-        # data =lo.experiment("c-{0}".format(c_level),no_trials=100,corruption_level=c_level)
-        # t= Test()
-        lo.iterative_algorithm("top_20_epochs_50_knapsack_{0}".format(i),pop_size=10000,genome_length=105,lim=20,trials=50,corruption_level=0.2,num_epochs=50)
+        for lim_percentage in [10,20,-20]:
+            if lim_percentage < 0:
+                lim_percentage = 0
+                lim = 20
+            name = "knapsack_105_items_{0}_{1}_{2}_{3}_{4}_{5}".format(pop_size,lim_percentage,num_epochs,lr,online_training,i)
+            lo = ACSKnapsack(corruption_level=corruption_level,knapsack_file="weing8.pkl")
+            # data =lo.experiment("c-{0}".format(c_level),no_trials=100,corruption_level=c_level)
+            # t= Test()
+            lo.iterative_algorithm(name,
+                                    pop_size=pop_size,
+                                    genome_length=genome_length,
+                                    lim_percentage=lim_percentage,
+                                    lim=lim,
+                                    trials=10,
+                                    corruption_level=corruption_level,
+                                    num_epochs=num_epochs,
+                                    lr = lr,
+                                    online_training=online_training,
+                                    pickle_data=False)
---
+++ run_experiments.sh
@@ -0,0 +1,12 @@
+
+
+for pop_size in 10000 5000 1000
+do
+	for num_epochs in 25 50 100 200
+	do
+		for lr in 0.001 0.01 0.1
+		do
+			screen python ga.py pop_size 20 20 num_epochs lr 0 &
+		done
+	done
+done
