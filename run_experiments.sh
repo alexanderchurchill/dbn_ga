21a388b158f1c380c085e8d3f29ef9123f95b291
alexanderchurchill@gmail.com <alexanderchurchill@gmail.com>
2014-02-11 16:02:12 +0000

    new_experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,12 +1,15 @@
 
 
-for pop_size in 10000 5000 1000
+for pop_size in 1000 5000 10000
 do
-	for num_epochs in 25 50 100 200
+	for num_epochs in 50
 	do
-		for lr in 0.001 0.01 0.1
+		for lr in 0.1 0.01 0.001
 		do
-			nohup python ga.py $pop_size 20 20 $num_epochs $lr 0 &
+			for corruption_level in 0.1 0.2 0.3
+			do
+				nohup python ga.py $pop_size 20 20 $num_epochs $lr 0 $corruption_level &
+			done
 		done
 	done
 done
